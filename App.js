9b90504b309af1b0582c68163a0fbc72b2495a24
nightness <joshguyette@yahoo.com>
2021-02-10 12:54:07 -0600

    moved navigation out of App.js to components/Navigation.js

--- App.js
+++ App.js
@@ -1,106 +1,21 @@
 import 'react-native-gesture-handler'
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState } from 'react'
 import { ModalPortal } from 'react-native-modals'
-import { NavigationContainer, DarkTheme, DefaultTheme } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { createDrawerNavigator } from '@react-navigation/drawer'
 import { AppLoading } from 'expo'
-import * as Notifications from 'expo-notifications';
 import * as Fonts from 'expo-font'
-import { ActivityIndicator, DisplayError } from './components/themed/Components'
-import { useAuthState } from './components/firebase/Firebase'
 import { FirebaseProvider } from './components/firebase/FirebaseContext'
-import { GlobalContext, GlobalProvider } from './components/shared/GlobalContext'
-import { ProfileContext, ProfileProvider } from './components/shared/ProfileContext'
+import { GlobalProvider } from './components/shared/GlobalContext'
 import { Defaults } from './components/shared/Constants'
-import Home from './components/Home'
-import Messenger from './components/messenger/Messenger'
-import DispatchMessenger from './components/messenger/DispatchMessenger'
-import ManageDistricts from './components/messenger/ManageDistricts'
 import Playground from './components/Playground'
-import Authentication from './components/Authentication'
+import AppNavigator from './components/Navigation'
 
 const getFonts = () => Fonts.loadAsync({
     'serif-pro-black': require('./assets/fonts/SourceSerifPro/SourceSerifPro-Black.ttf'),
     'serif-pro-bold': require('./assets/fonts/SourceSerifPro/SourceSerifPro-Bold.ttf')
 })
 
-const Drawer = createDrawerNavigator()
-
-const DrawerNavigator = () => {
-    return (
-        <ProfileProvider>
-            <Drawer.Navigator
-            //drawerContent={props => <DrawerContent {...props} />}
-            >
-                <Drawer.Screen
-                    name="Home"
-                    component={Home}
-                />
-                <Drawer.Screen
-                    name="Messenger"
-                    component={Messenger}
-                />
-                <Drawer.Screen
-                    name="Dispatch Messenger"
-                    component={DispatchMessenger}
-                />
-                <Drawer.Screen
-                    name="Manage Districts"
-                    component={ManageDistricts}
-                />
-            </Drawer.Navigator>
-        </ProfileProvider>
-    )
-}
-
-const Stack = createStackNavigator()
-const AppStack = () => {
-    const [user, firebaseLoading, firebaseError] = useAuthState()
-    const { theme, setTheme } = useContext(GlobalContext)
-
-
-    if (firebaseLoading) {
-        return (
-            <ActivityIndicator />
-        )
-    }
-    else if (firebaseError) {
-        return (
-            <DisplayError errorMessage={"Firebase Error: " + firebaseError.message} />
-        )
-    }
-    else {
-        return (
-            <NavigationContainer
-                theme={theme === "Dark" ? DarkTheme : DefaultTheme}
-            >
-                <Stack.Navigator
-                    headerMode="none"
-                    initialRouteName={user ? "Main" : "Authentication"}
-                    navigationOptions={({ navigation }) => ({
-                        headerLeft: null
-                    })}>
-                    <Stack.Screen name="Authentication" component={Authentication} />
-                    <Stack.Screen name="Main" component={DrawerNavigator} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        )
-    }
-}
-
-// ToDo: Register for Notifications
-const register = async () => {
-    //const { status } = await Notifications.
-    //console.log(status)
-}
-
 export default function App() {
     const [fontsLoaded, setFontsLoaded] = useState(false)
-
-    useEffect(() => {
-        //register()
-    }, [])
 
     if (!fontsLoaded) {
         return (
@@ -114,7 +29,7 @@ export default function App() {
         return (
             <GlobalProvider>
                 <FirebaseProvider>
-                    {(Defaults.playgroundMode ? <Playground /> : <AppStack />)}
+                    {(Defaults.playgroundMode ? <Playground /> : <AppNavigator />)}
                     <ModalPortal />
                 </FirebaseProvider>
             </GlobalProvider>
---
+++ components/Navigation.js
@@ -0,0 +1,79 @@
+import 'react-native-gesture-handler'
+import React, { useContext } from 'react'
+import { NavigationContainer, DarkTheme, DefaultTheme } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { ActivityIndicator, DisplayError } from './themed/Components'
+import { useAuthState } from './firebase/Firebase'
+import { GlobalContext } from './shared/GlobalContext'
+import { ProfileProvider } from './shared/ProfileContext'
+import Home from './Home'
+import Messenger from './messenger/Messenger'
+import DispatchMessenger from './messenger/DispatchMessenger'
+import ManageDistricts from './messenger/ManageDistricts'
+import Authentication from './Authentication'
+
+const Drawer = createDrawerNavigator()
+
+const DrawerNavigator = () => {
+    return (
+        <ProfileProvider>
+            <Drawer.Navigator
+            //drawerContent={props => <DrawerContent {...props} />}
+            >
+                <Drawer.Screen
+                    name="Home"
+                    component={Home}
+                />
+                <Drawer.Screen
+                    name="Messenger"
+                    component={Messenger}
+                />
+                <Drawer.Screen
+                    name="Dispatch Messenger"
+                    component={DispatchMessenger}
+                />
+                <Drawer.Screen
+                    name="Manage Districts"
+                    component={ManageDistricts}
+                />
+            </Drawer.Navigator>
+        </ProfileProvider>
+    )
+}
+
+const Stack = createStackNavigator()
+export default () => {
+    const [user, firebaseLoading, firebaseError] = useAuthState()
+    const { theme, setTheme } = useContext(GlobalContext)
+
+
+    if (firebaseLoading) {
+        return (
+            <ActivityIndicator />
+        )
+    }
+    else if (firebaseError) {
+        return (
+            <DisplayError errorMessage={"Firebase Error: " + firebaseError.message} />
+        )
+    }
+    else {
+        return (
+            <NavigationContainer
+                theme={theme === "Dark" ? DarkTheme : DefaultTheme}
+            >
+                <Stack.Navigator
+                    headerMode="none"
+                    initialRouteName={user ? "Main" : "Authentication"}
+                    navigationOptions={({ navigation }) => ({
+                        headerLeft: null
+                    })}>
+                    <Stack.Screen name="Authentication" component={Authentication} />
+                    <Stack.Screen name="Main" component={DrawerNavigator} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        )
+    }
+}
+
