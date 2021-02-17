f9f5e33aec08cac53875e36e8d0545f48ac2ce4e
nightness <joshguyette@yahoo.com>
2021-02-17 16:20:05 -0600

    refactoring

--- components/themed/LabeledSwitch.js
+++ components/themed/LabeledSwitch.js
@@ -4,13 +4,12 @@ import View from './View'
 import Switch from './Switch'
 import Text from './Text'
 
-export default ({ label, style, viewStyle, textStyle, key, onChange, ...restProps }) => {
+export default ({ label, style, viewStyle, textStyle, ...restProps }) => {
     return (
         <View style={[Styles.views.flexRowJustifyCenter, viewStyle]}>
             {label && <Text style={[{ marginRight: 10 }, textStyle]}>{label}</Text>}
             <Switch
                 style={style}
-                onChange={onChange}
                 {...restProps}
             />
         </View>
