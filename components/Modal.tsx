88eaf4f0e55ccfb745d1004382102aa0526391f4
nightness <joshguyette@yahoo.com>
2021-03-01 14:36:23 -0600

    JavaScript to TypeScript conversion

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Modal, ModalContent } from 'react-native-modals'
+import Modal, { ModalContent } from 'react-native-modals'
 import { GlobalContext } from '../shared/GlobalContext'
 import { Styles } from '../shared/Styles'
 import { Themes } from '../shared/Themes'
@@ -8,13 +8,14 @@ import { StyleProp, ViewStyle } from 'react-native'
 interface Props {
     children: JSX.Element[]
     style?: StyleProp<ViewStyle>
+    visible: boolean
 }
 
-export default ({ children, style, ...restProps }: Props) => {
+export default ({ children, style, visible = false, ...restProps }: Props) => {
     const { theme } = useContext(GlobalContext)
 
     return (
-        <Modal {...restProps}>
+        <Modal visible={visible} {...restProps}>
             <ModalContent style={[Styles.modal.content, Themes.modal[theme], style]}>
                 {children}
             </ModalContent>
