16a93ca1821f49fd1053079925acf31d65567b8b
nightness <joshguyette@yahoo.com>
2021-03-11 19:16:40 -0600

    snapshot

--- navigation/DrawerContent.tsx
+++ navigation/DrawerContent.tsx
@@ -13,13 +13,16 @@ import {
 } from '@react-navigation/drawer'
 import { Icon } from 'react-native-elements'
 import DrawerContentItem from './DrawerContentItem'
+import { GlobalContext } from '../shared/GlobalContext'
 
 const randomColor = () => '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')
 // Homer photoURL: https://yt3.ggpht.com/yti/ANoDKi75CjXyn5Omt5Z22dCgKdM_Ey2f9nraM4bYrxuu3A=s88-c-k-c0x00ffffff-no-rj-mo
 
 export const DrawerContent = ({ navigation, ...restProps }: DrawerContentComponentProps) => {
     // Need to grab the theme to determine the best background image
+    // This is not hookable 
     // Like ../assets/ddd0da2a.png for dark mode
+    // Like ../assets/dd426684.png for light mode
 
     return (
         <ImageBackground
