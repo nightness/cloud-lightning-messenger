15e8def4e62d2b50956bb0719bd80fbe3f272084
nightness <joshguyette@yahoo.com>
2021-06-02 14:42:15 -0500

    refactoring

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -81,37 +81,33 @@ interface Props {
 }
 
 export default ({ messages, setMessages }: Props) => {
-
     return (
-        <>
-            <View
-                style={{
-                    position: 'absolute',
-                    bottom: 45,
-                    left: 0,
-                    right: 0,
-                }}
-            >
-                {/* {messages.slice(0).reverse().map((message) => ( */}
-                {messages.slice(0).reverse().map((result) => {
-                    const [title, message] = result.split('`')
-                    return (
-                        <Message
-                            key={result}
-                            message={message}
-                            title={title}
-                            onHide={() => {
-                                setMessages((messages) =>
-                                    messages.filter(
-                                        (currentMessage) =>
-                                            currentMessage !== result
-                                    )
-                                );
-                            }}
-                        />
-                    )
-                })}
-            </View>
-        </>
+        <View
+            style={{
+                position: 'absolute',
+                bottom: 45,
+                left: 0,
+                right: 0,
+            }}
+        >
+            {messages.slice(0).reverse().map((result) => {
+                const [title, message] = result.split('`')
+                return (
+                    <Message
+                        key={result}
+                        message={message}
+                        title={title}
+                        onHide={() => {
+                            setMessages((messages) =>
+                                messages.filter(
+                                    (currentMessage) =>
+                                        currentMessage !== result
+                                )
+                            );
+                        }}
+                    />
+                )
+            })}
+        </View>
     );
 };
