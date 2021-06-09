32c0b0217dedfc30142ad9e5ec9784aa4eb25db0
nightness <joshguyette@yahoo.com>
2021-06-08 21:39:32 -0500

    snapshot

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react'
-import { Button, StyleSheet, StyleProp, TouchableOpacity, ViewStyle, View, SafeAreaView } from 'react-native'
+import { Button, StyleSheet, StyleProp, TouchableOpacity, ViewStyle, View } from 'react-native'
 import Text from './Text'
 import { ThemeContext } from './ThemeContext'
 import { Styles } from '../app/Styles'
@@ -28,23 +28,21 @@ export default ({
     // Use the native button
     if (reactNativeButton && title) {
         return (
-            <SafeAreaView style={style}>
+            <View style={style}>
                 <Button title={title} disabled={disabled} onPress={onPress} />
-            </SafeAreaView>
+            </View>
         )
     }
     // TouchableHighlight is another option, this works nice though
     return (
-        <SafeAreaView>
-            <TouchableOpacity
-                disabled={disabled}
-                style={[Styles.button.touchable, currentThemeType[activeTheme], style,]}
-                onPress={onPress}
-                {...restProps}
-            >
-                {title ? <Text style={[Styles.button.text, currentThemeType[activeTheme]]}>{title}</Text> : children}
-            </TouchableOpacity>
-        </SafeAreaView>
+        <TouchableOpacity
+            disabled={disabled}
+            style={[Styles.button.touchable, currentThemeType[activeTheme], style]}
+            onPress={onPress}
+            {...restProps}
+        >
+            {title ? <Text style={[Styles.button.text, currentThemeType[activeTheme]]}>{title}</Text> : children}
+        </TouchableOpacity>
     )
 }
 
